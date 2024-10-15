13ccc43d68df0ff7dfd3e028fd06c53cb525e1a1
kate0626 <101503385+Maikooon@users.noreply.github.com>
2024-10-16 00:49:52 +0900

    Update 20241012155349_create_users.rb

--- db/migrate/20241012155349_create_users.rb
+++ db/migrate/20241012155349_create_users.rb
@@ -7,3 +7,16 @@ class CreateUsers < ActiveRecord::Migration[7.2]
     end
   end
 end
+
+require 'database_cleaner'
+
+class ActiveSupport::TestCase
+  setup do
+    DatabaseCleaner.strategy = :transaction
+    DatabaseCleaner.start
+  end
+
+  teardown do
+    DatabaseCleaner.clean
+  end
+end
